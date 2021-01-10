c74a83d91ecbd4862b7dd5a3c8dbfe54f5802f2a
myrddral <thirdeye0@gmail.com>
2021-01-10 19:46:38 +0100

    new tasks added

---
+++ JS-workshop/fleet-of-things/fleetOfThings.js
@@ -0,0 +1,15 @@
+import { Thing } from './thing.hu.js';
+import { Fleet } from './fleet.hu.js';
+
+let fleet = new Fleet();
+
+// -  Van egy `Thing` osztályod
+// -  Van egy `Fleet` osztályod
+// - Ezen osztályokat használva érd el az alábbi kimenetet:
+
+// 1. [ ] Get milk
+// 2. [ ] Remove the obstacles
+// 3. [x] Stand up
+// 4. [x] Eat lunch
+
+console.log(fleet.toString());
